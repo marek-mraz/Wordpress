36689792a4e707502292f0bfa33a70610361b204
marek-mraz <contact@marek-mraz.com>
2026-04-04 10:52:12 +0200

    fixed

--- plugins/mu-plugins/oidc.php
+++ plugins/mu-plugins/oidc.php
@@ -27,44 +27,48 @@ if (get_env_value('OIDC_CLIENT_ID')) {
     if (!defined('OIDC_LINK_EXISTING_USERS')) define('OIDC_LINK_EXISTING_USERS', get_env_value('OIDC_LINK_EXISTING_USERS') === 'true');
     if (!defined('OIDC_CREATE_IF_DOES_NOT_EXIST')) define('OIDC_CREATE_IF_DOES_NOT_EXIST', get_env_value('OIDC_CREATE_IF_NOT_EXISTS') === 'true');
     if (!defined('OIDC_REDIRECT_USER_BACK')) define('OIDC_REDIRECT_USER_BACK', get_env_value('OIDC_REDIRECT_BACK') === 'true');
+    if (!defined('OIDC_ENFORCE_PRIVACY')) define('OIDC_ENFORCE_PRIVACY', get_env_value('OIDC_ENFORCE_PRIVACY') === 'true');
 }
 
-// 3. Inject Values into the Settings Object
-add_filter('openid-connect-generic-settings', 'oidc_inject_settings');
+// 3. Inject Values into the Settings Object via WP Core Option Filters
+add_filter('option_openid_connect_generic_settings', 'oidc_inject_settings');
+add_filter('default_option_openid_connect_generic_settings', 'oidc_inject_settings');
 
 function oidc_inject_settings($settings) {
+    if (!is_array($settings)) {
+        $settings = array();
+    }
 
     // If we can't find the Client ID, return the object untouched to prevent errors
     if (!get_env_value('OIDC_CLIENT_ID')) {
         return $settings;
     }
 
-    // --- KEY CHANGE: Use -> instead of [''] ---
-
-    $settings->client_id       = get_env_value('OIDC_CLIENT_ID');
-    $settings->client_secret   = get_env_value('OIDC_CLIENT_SECRET');
-    $settings->scope           = get_env_value('OIDC_SCOPE');
+    $settings['client_id']       = get_env_value('OIDC_CLIENT_ID');
+    $settings['client_secret']   = get_env_value('OIDC_CLIENT_SECRET');
+    $settings['scope']           = get_env_value('OIDC_SCOPE');
     
-    $settings->endpoint_login       = get_env_value('OIDC_ENDPOINT_LOGIN');
-    $settings->endpoint_userinfo    = get_env_value('OIDC_ENDPOINT_USERINFO');
-    $settings->endpoint_token       = get_env_value('OIDC_ENDPOINT_TOKEN');
-    $settings->endpoint_end_session = get_env_value('OIDC_ENDPOINT_LOGOUT');
+    $settings['endpoint_login']       = get_env_value('OIDC_ENDPOINT_LOGIN');
+    $settings['endpoint_userinfo']    = get_env_value('OIDC_ENDPOINT_USERINFO');
+    $settings['endpoint_token']       = get_env_value('OIDC_ENDPOINT_TOKEN');
+    $settings['endpoint_end_session'] = get_env_value('OIDC_ENDPOINT_LOGOUT');
 
-    $settings->identity_key       = get_env_value('OIDC_IDENTITY_KEY');
-    $settings->nickname_key       = get_env_value('OIDC_NICKNAME_KEY');
-    $settings->email_format       = get_env_value('OIDC_EMAIL_FORMAT');
-    $settings->displayname_format = get_env_value('OIDC_DISPLAYNAME_FORMAT');
+    $settings['identity_key']       = get_env_value('OIDC_IDENTITY_KEY');
+    $settings['nickname_key']       = get_env_value('OIDC_NICKNAME_KEY');
+    $settings['email_format']       = get_env_value('OIDC_EMAIL_FORMAT');
+    $settings['displayname_format'] = get_env_value('OIDC_DISPLAYNAME_FORMAT');
 
     // Handle Booleans
-    $settings->link_existing_users      = get_env_value('OIDC_LINK_EXISTING_USERS') === 'true' ? 1 : 0;
-    $settings->create_if_does_not_exist = get_env_value('OIDC_CREATE_IF_NOT_EXISTS') === 'true' ? 1 : 0;
-    $settings->redirect_user_back       = get_env_value('OIDC_REDIRECT_BACK') === 'true' ? 1 : 0;
-    $settings->identify_with_username   = get_env_value('OIDC_IDENTIFY_WITH_USERNAME') === 'true' ? 1 : 0;
-    $settings->token_refresh_enable     = get_env_value('OIDC_ENABLE_REFRESH_TOKEN') === 'true' ? 1 : 0;
+    $settings['link_existing_users']      = get_env_value('OIDC_LINK_EXISTING_USERS') === 'true' ? 1 : 0;
+    $settings['create_if_does_not_exist'] = get_env_value('OIDC_CREATE_IF_NOT_EXISTS') === 'true' ? 1 : 0;
+    $settings['redirect_user_back']       = get_env_value('OIDC_REDIRECT_BACK') === 'true' ? 1 : 0;
+    $settings['identify_with_username']   = get_env_value('OIDC_IDENTIFY_WITH_USERNAME') === 'true' ? 1 : 0;
+    $settings['token_refresh_enable']     = get_env_value('OIDC_ENABLE_REFRESH_TOKEN') === 'true' ? 1 : 0;
+    $settings['enforce_privacy']          = get_env_value('OIDC_ENFORCE_PRIVACY') === 'true' ? 1 : 0;
 
-    $settings->login_type = 'button';
-    $settings->http_request_timeout = 5;
-    $settings->no_sslverify = 0;
+    $settings['login_type'] = 'button';
+    $settings['http_request_timeout'] = 5;
+    $settings['no_sslverify'] = 0;
 
     return $settings;
 }
